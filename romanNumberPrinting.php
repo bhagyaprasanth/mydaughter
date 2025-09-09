567b67ca814442eb41c8de5f9fe73de30119802c
bhagyaprasanth <90758684+bhagyaprasanth@users.noreply.github.com>
2025-09-09 11:54:26 +0530

    Add files via upload

---
+++ romanNumberPrinting.php
@@ -0,0 +1,33 @@
+<?php
+
+function printRomanNumerals($number) {
+    $romanNumerals = array(
+        1000 => 'M',
+        900 => 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I'
+    );
+
+    $result = '';
+    foreach ($romanNumerals as $value => $numeral) {
+        while ($number >= $value) {
+            $result .= $numeral;
+            $number -= $value;
+        }
+    }
+
+    return $result;
+}
+for($inc=0;$inc<10;$inc++){
+    $number= random_int(0,3000);
+echo $number."-".printRomanNumerals($number)."<br/>";
+}
